d541878212658373f60ccd7ba924f45ad7318a63
C2unick <33051621+C2unick@users.noreply.github.com>
2021-05-29 21:33:03 +0800

    Update funddata.js

--- fund/js/funddata.js
+++ fund/js/funddata.js
@@ -5,4 +5,24 @@ var fundinfos = [
 		holdPrice: new BigNumber(2),
 		holdNum: new BigNumber(360),
 	},
+	{
+		code: "005827",
+		holdPrice: new BigNumber(2),
+		holdNum: new BigNumber(360),
+	},
+	{
+		code: "005827",
+		holdPrice: new BigNumber(2),
+		holdNum: new BigNumber(360),
+	},
+	{
+		code: "005827",
+		holdPrice: new BigNumber(2),
+		holdNum: new BigNumber(360),
+	},
+	{
+		code: "005827",
+		holdPrice: new BigNumber(2),
+		holdNum: new BigNumber(360),
+	},
 ];
